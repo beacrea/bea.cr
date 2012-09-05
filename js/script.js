174a7417b98dacdd8df5cb39d20318d3aa646652
Coty Beasley <design@beasleycreative.com>
2012-09-04 22:43:36 -0500

    Initial Add
    The current site as of September 4th, 2012 @ 10:43pm.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* Author: Coty Beasley
+
+*/
+
+$("#introBox h1").fitText(1.5, { minFontSize: '2.5em', maxFontSize: '100px' });
+$("#introBox h2").fitText(3, { minFontSize: '1.5em', maxFontSize: '50px' });
+$("#introBox a").fitText(.8, { minFontSize: '2em', maxFontSize: '50px' });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
